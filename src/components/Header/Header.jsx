231a0207ba7ed3bc0cca581900f742fbdef0ad62
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-22 00:43:59 +0600

    used Active Route with NavLink, useNavigation for loading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
-
-import { Link } from "react-router-dom";
+import './Header.css';
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+
   return (
     <nav className="text-2xl flex gap-6">
         <Link to={"/"}>Home</Link>
-        <Link to={"/users"}>Users</Link>
-        <Link to={"/about"}>About</Link>
+        {/* <Link to={"/users"}>Users</Link> */}
+
+        <NavLink to={"/users"}>Users</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+
+        {/* <Link to={"/about"}>About</Link> */}
         <Link to={"/contact"}>Contact Us</Link>
     </nav>
   );
